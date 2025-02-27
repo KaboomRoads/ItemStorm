227db04a8181f6203bdc7e84bfa94c7912b01f62
jashik1 <brabcja@gmail.com>
2025-02-27 11:01:24 +0100

    mixin

---
+++ src/main/java/net/itemstorm/mixin/ServerLevelMixin.java
@@ -0,0 +1,40 @@
+package net.itemstorm.mixin;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.storage.WritableLevelData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+
+@Mixin(ServerLevel.class)
+public abstract class ServerLevelMixin extends Level {
+
+
+    @Shadow
+    public abstract List<ServerPlayer> players();
+
+    protected ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+        super(levelData, dimension, registryAccess, dimensionTypeRegistration, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tickItemStorm(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
+        if (getGameTime() % 200 == 0) {
+            for (ServerPlayer player : players()) {
+//               player.getInventory().add()
+            }
+        }
+    }
+}
